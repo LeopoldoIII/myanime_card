ba6701306397b8203ff314a9ac98a9871572375c
Polo <nathan.rx@gmail.com>
2021-12-27 23:15:12 -0600

    Stack positioned

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,26 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: Stack(
+            overflow: Overflow.visible,
+            alignment: Alignment.topCenter,
             children: [
-              Container(
-                color: Colors.cyan,
-                height: 300.0,
-                width: 300.0,
-              ),
-              Container(
-                color: Colors.black,
-                height: 100.0,
-                width: 100.0,
+              Image.network(
+                  'https://http2.mlstatic.com/D_NQ_NP_2X_984723-MLM44200016908_112020-F.webp'),
+              Positioned(
+                bottom: 0.0,
+                right: 0.0,
+                child: Container(
+                  color: Colors.black,
+                  height: 40.0,
+                  child: const Text(
+                    'Frozen 2',
+                    style: TextStyle(
+                      color: Colors.white,
+                      letterSpacing: 3.0,
+                      fontSize: 30.0,
+                    ),
+                  ),
+                ),
               )
             ],
           ),
