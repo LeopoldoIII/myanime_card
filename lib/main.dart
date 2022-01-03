7249471c4989ca0c60d425a0c1ce47958ef520ed
Polo <nathan.rx@gmail.com>
2022-01-02 20:56:34 -0600

    Example Text

--- lib/main.dart
+++ lib/main.dart
@@ -9,32 +9,34 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return  MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Stack(
-            overflow: Overflow.visible,
-            alignment: Alignment.topCenter,
-            children: [
-              Image.network(
-                  'https://http2.mlstatic.com/D_NQ_NP_2X_984723-MLM44200016908_112020-F.webp'),
-              Positioned(
-                bottom: 0.0,
-                right: 0.0,
-                child: Container(
+          child: Text(
+            'Hello World', 
+            style: TextStyle(
+              fontSize: 40.0,
+              color: Colors.cyan,
+              fontWeight: FontWeight.w600,
+              decoration: TextDecoration.combine(
+                [
+                  TextDecoration.overline,
+                  TextDecoration.underline,
+                  TextDecoration.lineThrough
+                ],
+              ),
+              decorationColor: Colors.red,
+              decorationThickness: 1.0,
+              decorationStyle: TextDecorationStyle.dashed,
+              wordSpacing: 70.0,
+              shadows: [
+                Shadow(
                   color: Colors.black,
-                  height: 40.0,
-                  child: const Text(
-                    'Frozen 2',
-                    style: TextStyle(
-                      color: Colors.white,
-                      letterSpacing: 3.0,
-                      fontSize: 30.0,
-                    ),
-                  ),
-                ),
-              )
-            ],
+                  blurRadius: 9.0,
+                  offset: Offset(10.0,10.0),
+                )
+              ]
+            ),
           ),
         ),
       ),
